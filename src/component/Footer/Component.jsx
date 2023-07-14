0056e17678d63784e18439d842646c5b976be0c0
Shubham7270 <116951620+Shubham7270@users.noreply.github.com>
2023-07-14 13:43:10 +0530

    first commit

---
+++ src/component/Footer/Component.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import "./component.css";
+
+export function Component() {
+  return (
+    <div className="component">
+      <p className="signup-and-start">
+        Signup and start enjoying the benifits today!
+      </p>
+      <div className="sign-up-button">
+        <div className="overlap-group">
+          <div className="sign-up-to-lilypads">Sign Up To Lilypads</div>
+        </div>
+      </div>
+      <div className="newsketter">
+        <div className="overlap">
+          <div className="subscribe-to-wrapper">
+            <div className="subscribe-to">Subscribe To Newsletter</div>
+          </div>
+          <div className="enter-your-email">Enter Your Email Here</div>
+        </div>
+      </div>
+      <div className="facebook">
+        <img
+          className="img"
+          alt="Facebook circle fill"
+          src="facebook-circle-fill-1-1.svg"
+        />
+      </div>
+      <div className="twitter">
+        <img className="img" alt="Twitter fill" src="twitter-fill-1-1.svg" />
+      </div>
+      <div className="linkedin">
+        <img className="img" alt="Linkedin fill" src="linkedin-fill-1-1.svg" />
+      </div>
+      <img className="main-logo" alt="Main logo" src="main-logo-1@2x.png" />
+      <div className="network">Network</div>
+      <div className="community">Community</div>
+      <div className="text-wrapper">Investor</div>
+      <div className="broker">Broker</div>
+      <div className="product">Product</div>
+      <div className="reach-us-at">Reach Us At</div>
+      <div className="our-story">Our Story</div>
+      <div className="invest">Invest</div>
+      <div className="approach">Approach</div>
+      <div className="discover">Discover</div>
+      <div className="aboutus">AboutUs</div>
+      <div className="transact">Transact</div>
+      <div className="manage">Manage</div>
+      <div className="technology">Technology</div>
+      <div className="artificial">Artificial Intelligence</div>
+      <div className="block-chain">Block Chain</div>
+      <div className="div">Highlights</div>
+      <p className="impact-of-COVID">
+        Impact of COVID-19 on the food and beverage industry and the retail
+        sector
+      </p>
+      <p className="element-columbia">
+        6751 Columbia Gateway Dr.,&nbsp;&nbsp;
+        <br />
+        3rd floor, Columbia,&nbsp;&nbsp;
+        <br />
+        MD 21046
+      </p>
+      <p className="commercial-lending">
+        Commercial lending during COVID-19: navigating the impact
+      </p>
+      <div className="knowledge-center">Knowledge Center</div>
+      <div className="resource">Resource</div>
+      <div className="blog">Blog</div>
+      <img className="line" alt="Line" src="line-6-1.svg" />
+      <div className="youtube">
+        <img className="img" alt="Youtube fill" src="youtube-fill-1-1.svg" />
+      </div>
+      <div className="medium">
+        <img className="img" alt="Bxl medium" src="bxl-medium-1-1.svg" />
+      </div>
+      <div className="pinterest">
+        <img
+          className="img"
+          alt="Bxl pinterest alt"
+          src="bxl-pinterest-alt-1-1.svg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Component;
---
+++ src/component/LandingPage/LandingPage.jsx
@@ -0,0 +1,327 @@
+import React from "react";
+import "./landingpage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="div">
+        <div className="overlap">
+          <div className="ellipse" />
+          <img className="rectangle" alt="Rectangle" src="rectangle.png" />
+          <img className="img" alt="Rectangle" src="rectangle-1.png" />
+          <img className="group" alt="Group" src="group-236.png" />
+          <div className="text-wrapper">Network</div>
+          <div className="text-wrapper-2">Home</div>
+          <div className="text-wrapper-3">Product</div>
+          <div className="text-wrapper-4">Technology</div>
+          <div className="text-wrapper-5">Our Story</div>
+          <div className="text-wrapper-6">Knowledge Center</div>
+          <div className="ellipse-2" />
+          <div className="frame">
+            <h1 className="unprecedented-era-of">
+              <span className="span">Unprecedented Era of </span>
+              <span className="text-wrapper-7">Real Estate Investing</span>
+            </h1>
+            <p className="p">
+              Lilypads uses sophisticated technologies for data-driven decisions
+              in investing, managing and funding commercial real estate assets
+            </p>
+          </div>
+          <div className="frame-2">
+            <div className="text-wrapper-8">Start Investing Now</div>
+            <img
+              className="arrow-right-line"
+              alt="Arrow right line"
+              src="arrow-right-line-1.svg"
+            />
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-9">Log In</div>
+          </div>
+          <div className="frame-3">
+            <div className="text-wrapper-10">Contact Us</div>
+          </div>
+          <div className="frame-4">
+            <div className="frame-5">
+              <div className="text-wrapper-11">658+</div>
+              <div className="text-wrapper-12">Properties Registered</div>
+            </div>
+            <div className="frame-5">
+              <div className="text-wrapper-11">685+</div>
+              <div className="text-wrapper-12">Deals Cracked</div>
+            </div>
+            <div className="frame-5">
+              <div className="text-wrapper-11">255+</div>
+              <div className="text-wrapper-12">Investors</div>
+            </div>
+          </div>
+          <div className="text-wrapper-13">Welcome to the</div>
+          <div className="line"></div>
+          <div className="overlap-wrapper">
+            <div className="overlap-group">
+              <div className="group-2">
+                <div className="group-3" />
+                <div className="rectangle-wrapper">
+                  <div className="rectangle-2" />
+                </div>
+              </div>
+              <div className="overlap-2">
+                <div className="group-4">
+                  <div className="group-5" />
+                  <div className="group-6">
+                    <div className="rectangle-3" />
+                  </div>
+                </div>
+                <div className="text-wrapper-14">Committed</div>
+                <div className="text-wrapper-15">Committed</div>
+                <div className="text-wrapper-16">Contribution</div>
+                <div className="text-wrapper-17">Contribution</div>
+                <div className="text-wrapper-18">Distribution</div>
+                <div className="text-wrapper-19">Distribution</div>
+                <div className="text-wrapper-20">$219,278</div>
+                <div className="text-wrapper-21">$169.43</div>
+                <div className="text-wrapper-22">$710,568</div>
+                <div className="text-wrapper-23">$854.08</div>
+                <div className="text-wrapper-24">$928,541</div>
+                <div className="text-wrapper-25">$275.43</div>
+                <div className="element-yield">
+                  <span className="text-wrapper-26">+ 3,5% </span>
+                  <span className="text-wrapper-27">Yield</span>
+                </div>
+                <img className="line-2" alt="Line" src="line-38.svg" />
+                <img className="line-3" alt="Line" src="line-44.svg" />
+                <img className="line-4" alt="Line" src="line-46.svg" />
+                <img className="line-2" alt="Line" src="line-42.svg" />
+                <img className="line-3" alt="Line" src="line-45.svg" />
+                <img className="line-4" alt="Line" src="line-47.svg" />
+                <div className="group-7">
+                  <div className="ellipse-wrapper">
+                    <div className="ellipse-3" />
+                  </div>
+                  <img className="group-8" alt="Group" src="group-4.png" />
+                  <img className="group-9" alt="Group" src="group-1.png" />
+                  <img className="group-10" alt="Group" src="group-1.png" />
+                  <img className="group-11" alt="Group" src="group-3.png" />
+                </div>
+                <p className="text-wrapper-28">
+                  164 S.carson court newport news,VA 23601
+                </p>
+                <img
+                  className="rectangle-4"
+                  alt="Rectangle"
+                  src="rectangle-2894.png"
+                />
+                <div className="text-wrapper-29">Genisis</div>
+              </div>
+              <div className="element-yield-2">
+                <span className="text-wrapper-26">+ 3,5% </span>
+                <span className="text-wrapper-27">Yield</span>
+              </div>
+              <img
+                className="line-graph"
+                alt="Line graph"
+                src="line-graph.png"
+              />
+              <div className="text-wrapper-30">Investments</div>
+              <p className="text-wrapper-31">
+                164 S.carson court newport news,VA 23601
+              </p>
+              <img
+                className="rectangle-5"
+                alt="Rectangle"
+                src="rectangle-2641.svg"
+              />
+              <div className="text-wrapper-32">Aurora</div>
+            </div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-3">
+              <div className="overlap-4">
+                <div className="overlap-5">
+                  <img
+                    className="rectangle-6"
+                    alt="Rectangle"
+                    src="rectangle-2597.svg"
+                  />
+                  <img
+                    className="ellipse-4"
+                    alt="Ellipse"
+                    src="ellipse-38.svg"
+                  />
+                </div>
+                <div className="text-wrapper-33">Julie Craig</div>
+                <div className="text-wrapper-34">Investor</div>
+              </div>
+              <div className="overlap-6">
+                <div className="text-wrapper-35">Borrowed</div>
+                <div className="text-wrapper-36">Earned</div>
+                <div className="text-wrapper-37">Paid</div>
+                <div className="text-wrapper-38">Loan Term</div>
+                <div className="text-wrapper-39">19 Collaborators</div>
+                <div className="rectangle-7" />
+                <div className="rectangle-8" />
+                <div className="element-deals">
+                  <span className="text-wrapper-40">
+                    68
+                    <br />
+                  </span>
+                  <span className="text-wrapper-41">Deals</span>
+                </div>
+                <div className="text-wrapper-42">$ 350,000</div>
+                <div className="text-wrapper-43">$ 166,000</div>
+                <div className="text-wrapper-44">$ 184,000</div>
+                <div className="text-wrapper-45">12</div>
+                <div className="element-match">
+                  <span className="text-wrapper-46">
+                    80%
+                    <br />
+                  </span>
+                  <span className="text-wrapper-41">Match</span>
+                </div>
+                <div className="text-wrapper-47">62%</div>
+                <div className="text-wrapper-48">38%</div>
+                <div className="text-wrapper-49">Months</div>
+                <div className="ellipse-5" />
+                <div className="ellipse-6" />
+                <div className="group-12">
+                  <div className="overlap-group-2">
+                    <div className="ellipse-3" />
+                  </div>
+                  <img className="group-8" alt="Group" src="group-4.png" />
+                  <img className="group-9" alt="Group" src="group-5.png" />
+                  <img className="group-10" alt="Group" src="group-6.png" />
+                  <img className="group-11" alt="Group" src="group-7.png" />
+                </div>
+                <img
+                  className="briefcase"
+                  alt="Briefcase"
+                  src="briefcase-24-1.svg"
+                />
+                <img
+                  className="user-heart-line"
+                  alt="User heart line"
+                  src="user-heart-line-1.svg"
+                />
+                <img className="line-5" alt="Line" src="line-15.svg" />
+                <div className="ellipse-7" />
+                <div className="text-wrapper-50">+14</div>
+                <img className="ellipse-8" alt="Ellipse" src="ellipse-50.svg" />
+                <img className="ellipse-9" alt="Ellipse" src="ellipse-49.svg" />
+                <img
+                  className="ellipse-10"
+                  alt="Ellipse"
+                  src="ellipse-48.svg"
+                />
+                <img
+                  className="ellipse-11"
+                  alt="Ellipse"
+                  src="ellipse-47.svg"
+                />
+                <img
+                  className="ellipse-12"
+                  alt="Ellipse"
+                  src="ellipse-46.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="group-13">
+            <div className="overlap-7">
+              <div className="rectangle-9" />
+              <div className="group-14">
+                <div className="rectangle-10" />
+                <div className="overlap-group-3">
+                  <div className="rectangle-3" />
+                </div>
+                <div className="text-wrapper-51">Committed</div>
+                <div className="text-wrapper-52">Contribution</div>
+                <div className="text-wrapper-53">Distribution</div>
+                <div className="text-wrapper-54">$105,555</div>
+                <div className="text-wrapper-55">$473,685</div>
+                <div className="text-wrapper-56">$948,355</div>
+                <div className="element-yield-3">
+                  <span className="text-wrapper-26">+ 3,5% </span>
+                  <span className="text-wrapper-27">Yield</span>
+                </div>
+                <img
+                  className="line-graph-2"
+                  alt="Line graph"
+                  src="line-graph.svg"
+                />
+                <p className="text-wrapper-57">
+                  164 S.carson court newport news,VA 23601
+                </p>
+                <img
+                  className="rectangle-11"
+                  alt="Rectangle"
+                  src="rectangle-2893.svg"
+                />
+                <div className="text-wrapper-58">Geminin Appartment</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Content Start */}
+        <div className="overlap-8">
+          <div className="rectangle-12" />
+          <img className="line-6" alt="Line" src="line-2.svg" />
+          <p className="text-wrapper-59">
+            Lillypad is the only Real Estate Investment tool you need for your
+            business
+          </p>
+          <div className="text-wrapper-60">About Us</div>
+          <div className="text-wrapper-61">Become a Data-Driven Investor</div>
+          <p className="text-wrapper-62">
+            Lilypads is the cloud-based real estate investment platform that
+            helps you make data-driven investment decisions and assists with
+            reaching your long term investment objectives.
+          </p>
+          <div className="text-wrapper-63">Manager Due Diligence</div>
+          <p className="text-wrapper-64">
+            Evaluate managers against your objectives using traditional risk
+            metrics with the addition of factor analysis. All it takes is a set
+            of returns
+          </p>
+          <p className="text-wrapper-65">
+            Lilypads can help you make smarter decisions.
+          </p>
+          <p className="text-wrapper-66">
+            Streamline your investment evaluation process and collaborate in
+            reaching your long term investment objectives
+          </p>
+          <div className="text-wrapper-67">Optimize Asset Allocation</div>
+          <p className="understand-the">
+            Understand the underlying risk drivers in your portfolio using the
+            Lilypads analysis factors&nbsp;&nbsp;and then optimize that
+            portfolio based on objectives
+          </p>
+          <div className="text-wrapper-68">Our Subscribers</div>
+          <p className="text-wrapper-69">
+            Serving a constellation of global subscribers Pension funds, Funds
+            of Funds, Banks, Family offices, Asset managers, Wealth Managers
+          </p>
+          <div className="group-wrapper">
+            <img className="group-15" alt="Group" src="group-48096377.png" />
+          </div>
+          <div className="img-wrapper">
+            <img className="group-15" alt="Group" src="group-48096378.png" />
+          </div>
+          <div className="group-16">
+            <img className="group-17" alt="Group" src="group-48096374.png" />
+          </div>
+          <div className="group-18">
+            <img className="group-19" alt="Group" src="group-48096376.png" />
+          </div>
+          <div className="group-20">
+            <img className="group-21" alt="Group" src="group-48096375.png" />
+          </div>
+          <img className="line-7" alt="Line" src="line-221.svg" />
+          <img className="line-8" alt="Line" src="line-223.svg" />
+          <img className="line-9" alt="Line" src="line-222.svg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
